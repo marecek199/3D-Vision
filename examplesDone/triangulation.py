3a6d060fe4dce36fcdb82f083eb6aa1a8787fce4
marekSoos <soos@smartvision.eu>
2026-07-05 09:29:13 +0200

    ADd trigulations

---
+++ examplesDone/triangulation.py
@@ -0,0 +1,35 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2 as cv
+
+f, cx, cy = 1000., 320., 240.
+pts0 = np.loadtxt('data/image_formation0.xyz')[:,:2]
+pts1 = np.loadtxt('data/image_formation1.xyz')[:,:2]
+output_file = 'triangulation.xyz'
+
+# Estimate relative pose of two view
+F, _ = cv.findFundamentalMat(pts0, pts1, cv.FM_8POINT)
+K = np.array([[f, 0, cx], [0, f, cy], [0, 0, 1]])
+E = K.T @ F @ K
+_, R, t, _ = cv.recoverPose(E, pts0, pts1)
+
+# Reconstruct 3D points (triangulation)
+P0 = K @ np.eye(3, 4, dtype=np.float32)
+Rt = np.hstack((R, t))
+P1 = K @ Rt
+X = cv.triangulatePoints(P0, P1, pts0.T, pts1.T)
+X /= X[3]
+X = X.T
+
+# Write the reconstructed 3D points
+np.savetxt(output_file, X)
+
+# Visualize the reconstructed 3D points
+ax = plt.figure(layout='tight').add_subplot(projection='3d')
+ax.plot(X[:,0], X[:,1], X[:,2], 'ro')
+ax.set_aspect('equal')
+ax.set_xlabel('X [m]')
+ax.set_ylabel('Y [m]')
+ax.set_zlabel('Z [m]')
+ax.grid(True)
+plt.show()
